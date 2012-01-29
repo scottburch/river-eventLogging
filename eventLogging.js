875fa64a363b7cd31c621b5680a0f3ef46086a1f
Scott Burch <scott@toto.bulldoginfo.com>
2012-01-29 10:18:41 -0800

    initial commit

---
+++ eventLogging.js
@@ -0,0 +1,6 @@
+defineModule({name:'eventLogging', category:'logging', description:'Event logging'}, function(that) {
+
+    that.on_moduleManager_eventFired = function(data) {
+        that.doAction('log', ['Event: '+data.moduleName, data.event, data.data]);
+    }
+});
